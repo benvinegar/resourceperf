4d4f9788f8baf60e666296a691cc8cd923a8c2c3
Ben Vinegar <ben@benv.ca>
2015-02-23 20:25:12 -0800

    Load each document 10 times; average results

--- public/javascripts/runner.js
+++ public/javascripts/runner.js
@@ -1,45 +1,63 @@
 (function () {
-  var iframe = $('<iframe>')
-    .hide()
-    .appendTo('body');
-  
   var documentData = $('#documents').text();
   var documents = JSON.parse(documentData);
 
-  var timings = [];
+  var allTimings = [];
 
-  function nextDocument(first) {
-    var next = documents.shift();
+  function avg(arr, key) {
+    return arr.reduce(function (r, x) {
+      return r + x[key];
+    }, 0) / arr.length;
+  }
 
-    if (!first)
-      // Use parse/stringiy to deep copy timing data
-      // (reference will be lost when we redirect the iframe)
-      timings.push(
-        JSON.parse(JSON.stringify(
-          iframe[0].contentWindow.performance.timing
-        ))
-      );
+  function nextDocument() {
 
-    if (!next) 
-      return void report();
+    var next = documents.shift(),
+      frameTimings = [],
+      count = 0;
 
-    iframe.attr('src', '/document/' + next.id);
+    var iframe = $('<iframe>')
+      .hide()
+      .appendTo('body')
+      .load(function () {
+        // Use parse/stringiy to deep copy timing data
+        // (reference will be lost when we redirect the iframe)
+        var win = iframe[0].contentWindow;
+        frameTimings.push(JSON.parse(JSON.stringify(
+          win.performance.timing
+        )));
+
+        if (count++ < 10)
+          win.location.reload();
+        else {
+
+          allTimings.push({
+            domComplete: frameTimings.reduce(function (r, t) {
+             return r + (t.domComplete - t.connectStart); 
+            }, 0) / count,
+            onload: frameTimings.reduce(function (r, t) {
+              return r + (t.loadEventEnd - t.connectStart);
+            }, 0) / count
+          });
+
+          if (documents.length)
+            nextDocument();
+          else
+            report();
+        }
+      })
+      .attr('src', '/document/' + next.id);
   }
 
   function report() {
     var domComplete = $('.js-dom-complete'),
       onload = $('.js-onload');
 
-    timings.forEach(function (timing, index) {
-      var start = timing.connectStart;
-      domComplete.eq(index).text(timing.domComplete - start);
-      onload.eq(index).text(timing.loadEventEnd - start);
+    allTimings.forEach(function (timing, index) {
+      domComplete.eq(index).text(timing.domComplete.toFixed(3));
+      onload.eq(index).text(timing.onload.toFixed(3));
     });
   }
-
-  iframe.on('load', function () {
-    nextDocument.call(this, false);
-  });
 
   $('#start').click(function () {
     nextDocument.call(this, true);
