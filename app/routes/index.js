ecbd5d6fa235357a2bb4085b7639b59aaa13c339
Ben Vinegar <ben@benv.ca>
2015-02-26 00:33:39 -0800

    Can now edit existing testcases

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,9 +5,14 @@ var models = require('../models');
 var Sequelize = require('sequelize');
 
 var jsesc = require('jsesc');
+var _ = require('lodash');
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Create a test' });
+  res.render('index', {
+    title: 'Create a test',
+    testcase: {},
+    documents: [{}, {}] // 2
+  });
 });
 
 router.post('/', function(req, res, next) {
@@ -32,7 +37,7 @@ router.post('/', function(req, res, next) {
 });
 
 
-router.get('/:slug', function(req, res, next) {
+router.get('/:slug', function (req, res, next) {
   models.TestCase.find({
     where: {slug: req.params.slug }
   }).then(function (testcase) {
@@ -42,7 +47,7 @@ router.get('/:slug', function(req, res, next) {
     }
 
     testcase.getDocuments().then(function (documents) {
-      res.render('testcase', {
+      res.render('testcase/show', {
         title: testcase.name,
         testcase: testcase,
         documents: documents,
@@ -50,6 +55,47 @@ router.get('/:slug', function(req, res, next) {
       });
     });
   });
+});
+
+router.get('/:slug/edit', function (req, res, next) {
+  models.TestCase.find({
+    where: {slug: req.params.slug }
+  }).then(function (testcase) {
+    if (!testcase) {
+      res.status(404);
+      return res.render('404');
+    }
+
+    testcase.getDocuments().then(function (documents) {
+      res.render('testcase/edit', {
+        title: testcase.name,
+        testcase: testcase,
+        documents: documents
+      });
+    });
+  });
+});
+
+router.post('/:slug/update', function (req, res, next) {
+  var chainer = new Sequelize.Utils.QueryChainer;
+
+  chainer.add(
+    models.TestCase.update(_.pick(req.body, ['name', 'desc', 'slug']), {
+      where: { id : req.body.id }
+    })
+  );
+
+  req.body.document.forEach(function (doc) {
+    chainer.add(models.Document.update(_.omit(doc, 'id'), {
+      where: { id : doc.id }
+    }));
+  });
+
+  chainer
+    .run()
+    .then(function () {
+      res.redirect('/' + req.body.slug);
+    });
 });
 
 router.get('/document/new', function (req, res, next) {
