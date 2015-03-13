daba2250a3e7b0d31a0e04a6cd6e926855740cae
Matt Robenolt <matt@ydekproductions.com>
2015-03-13 12:02:34 -0700

    Stub in a blank favicon

--- app/app.js
+++ app/app.js
@@ -16,8 +16,7 @@ app.set('view engine', 'hbs');
 var hbs = require('hbs');
 hbs.registerPartials(__dirname + '/../templates/partials');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(path.join(__dirname, '../public/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
