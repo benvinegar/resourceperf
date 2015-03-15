72420669eb055e36e661e0b2738604ed3bcfc50b
Ben Vinegar <ben@benv.ca>
2015-03-15 16:03:20 -0700

    head/body db column should be text, not string/varchar

--- app/models/document.js
+++ app/models/document.js
@@ -3,8 +3,8 @@
 module.exports = function(sequelize, DataTypes) {
   var Document = sequelize.define("Document", {
     title: DataTypes.STRING,
-    head: DataTypes.STRING,
-    body: DataTypes.STRING
+    head: DataTypes.TEXT,
+    body: DataTypes.TEXT
   }, {
     classMethods: {
       associate: (models) => Document.belongsTo(models.TestCase)
--- app/models/testcase.js
+++ app/models/testcase.js
@@ -4,7 +4,7 @@ module.exports = function(sequelize, DataTypes) {
   var TestCase = sequelize.define("TestCase", {
     name: DataTypes.STRING,
     slug: DataTypes.STRING,
-    desc: DataTypes.STRING
+    desc: DataTypes.TEXT
   }, {
     classMethods: {
       associate: (models) => TestCase.hasMany(models.Document)
